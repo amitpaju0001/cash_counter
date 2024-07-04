3f3b2fda2fe5203c15af53049c02c8027c37a56d
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-04 18:00:12 +0530

    commit

--- lib/login/ui/login_screen.dart
+++ lib/login/ui/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:cash_counter/dashboard/ui/dashboard_screen.dart';
 import 'package:cash_counter/login/model/user_model.dart';
 import 'package:cash_counter/login/provider/auth_provider.dart';
 import 'package:cash_counter/login/ui/register_screen.dart';
-import 'package:cash_counter/shared/app_colors.dart';
 import 'package:cash_counter/shared/app_string.dart';
 import 'package:cash_counter/shared/app_text_field.dart';
 import 'package:flutter/material.dart';
@@ -22,121 +21,145 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Consumer<AuthProvider>(
-          builder: (context, authProvider, widget) {
-            return Center(
-              child: SingleChildScrollView(
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Stack(
-                      children:[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              login,
-                              style: TextStyle(
-                                  fontSize: 28, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: Consumer<AuthProvider>(
+        builder: (context, authProvider, widget) {
+          return Center(
+            child: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Stack(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            login,
+                            style: TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueAccent),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            loginText,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey[700],
                             ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            const Text(
-                              loginText,
-                              style: TextStyle(
+                          ),
+                          const SizedBox(height: 32),
+                          Text(
+                            email,
+                            style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            const Text(
-                              email,
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            AppTextField(
-                              controller: emailController,
-                              hintText: emailFieldHint,
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            const Text(
-                              password,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            AppTextField(
-                              controller: passwordController,
-                              obscureText: authProvider.isVisible ? false : true,
-                              hintText: passwordFieldHint,
-                              suffixIcon: IconButton(
-                                icon: Icon(Icons.remove_red_eye),
-                                onPressed: () {
-                                  authProvider.setPasswordFieldStatus();
-                                },
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 24,
-                            ),
-                            InkWell(
-                              onTap: () {
-                                loginUser();
+                                color: Colors.grey[700]),
+                          ),
+                          const SizedBox(height: 8),
+                          AppTextField(
+                            controller: emailController,
+                            hintText: emailFieldHint,
+                            fillColor: Colors.grey[100],
+                            filled: true,
+                            borderRadius: 12,
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            password,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.grey[700]),
+                          ),
+                          const SizedBox(height: 8),
+                          AppTextField(
+                            controller: passwordController,
+                            obscureText: authProvider.isVisible ? false : true,
+                            hintText: passwordFieldHint,
+                            fillColor: Colors.grey[100],
+                            filled: true,
+                            borderRadius: 12,
+                            suffixIcon: IconButton(
+                              icon: Icon(authProvider.isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                              onPressed: () {
+                                authProvider.setPasswordFieldStatus();
                               },
-                              child: Container(
-                                alignment: Alignment.center,
-                                padding: const EdgeInsets.symmetric(vertical: 16),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: buttonBackground,
-                                  borderRadius: BorderRadius.circular(24),
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          InkWell(
+                            onTap: () {
+                              loginUser();
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                  colors: [
+                                    Colors.blueAccent,
+                                    Colors.lightBlueAccent
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
                                 ),
-                                child: const Text(
-                                  login,
-                                  style: TextStyle(color: buttonTextColor),
-                                ),
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                              child: const Text(
+                                login,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
                               ),
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(noAccount),
-                                const SizedBox(
-                                  width: 4,
+                          ),
+                          const SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(noAccount),
+                              const SizedBox(width: 4),
+                              TextButton(
+                                onPressed: () {
+                                  openRegisterUser();
+                                },
+                                child: const Text(
+                                  register,
+                                  style: TextStyle(
+                                      color: Colors.blueAccent,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                TextButton(
-                                    onPressed: () {
-                                      openRegisterUser();
-                                    },
-                                    child: const Text(
-                                      register,
-                                      style: TextStyle(color: textButtonColor),
-                                    ))
-                              ],
-                            )
-                          ],
-                        ),
-                      Positioned(child: authProvider.isLoading?CircularProgressIndicator():SizedBox()),
-                      ],
-                    ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Positioned(
+                        child: authProvider.isLoading
+                            ? const Center(child: CircularProgressIndicator())
+                            : const SizedBox(),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            );
-          },
-        ));
+            ),
+          );
+        },
+      ),
+    );
   }
 
   void openRegisterUser() {
@@ -147,15 +170,18 @@ class _LoginScreenState extends State<LoginScreen> {
     ));
   }
 
-  void loginUser() async{
+  void loginUser() async {
     UserModel user = UserModel(
         email: emailController.text, password: passwordController.text);
-    AuthProvider authProvider = Provider.of<AuthProvider>(context,listen: false);
-   bool isExist = await authProvider.isUserExists(user);
-   if(isExist&& mounted){
-     Navigator.push(context, MaterialPageRoute(builder: (context) {
-       return DashboardScreen();
-     },));
-   }
+    AuthProvider authProvider =
+        Provider.of<AuthProvider>(context, listen: false);
+    bool isExist = await authProvider.isUserExists(user);
+    if (isExist && mounted) {
+      Navigator.push(context, MaterialPageRoute(
+        builder: (context) {
+          return const DashboardScreen();
+        },
+      ));
+    }
   }
 }
--- lib/shared/app_text_field.dart
+++ lib/shared/app_text_field.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
 
 class AppTextField extends StatelessWidget {
-   const AppTextField({
+  const AppTextField({
+    this.fillColor,
+    this.filled,
+    this.borderRadius = 12,
     this.obscureText = false,
     this.controller,
-    super.key, this.hintText, this.onPressed, this.suffixIcon,this.keyboardType,});
+    super.key,
+    this.hintText,
+    this.onPressed,
+    this.suffixIcon,
+    this.keyboardType,
+  });
+
   final TextEditingController? controller;
   final bool obscureText;
   final String? hintText;
   final VoidCallback? onPressed;
-  final Widget?  suffixIcon;
-   final TextInputType? keyboardType;
+  final Widget? suffixIcon;
+  final TextInputType? keyboardType;
+  final Color? fillColor;
+  final bool? filled;
+  final double borderRadius;
+
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
+    return TextFormField(
       controller: controller,
       obscureText: obscureText,
-        keyboardType: keyboardType, textCapitalization: TextCapitalization.words,
+      keyboardType: keyboardType,
+      textCapitalization: TextCapitalization.words,
       decoration: InputDecoration(
-          isDense: true,
-          hintText: hintText,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(24),
-          ),
-          suffixIcon: suffixIcon
-          ),
+        isDense: true,
+        hintText: hintText,
+        fillColor: fillColor ?? Colors.grey[100],
+        filled: filled ?? true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: suffixIcon,
+      ),
       validator: (value) {
         if (value == null || value.isEmpty) {
           return "Please enter some text";
